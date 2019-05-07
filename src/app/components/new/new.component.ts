963cfec9b3205ecc9c06067f798d136087e9fa9c
Lucas Heim <lheim@asesconsulting.com>
2019-05-07 10:12:12 -0300

    open in browser plugin

--- src/app/components/new/new.component.ts
+++ src/app/components/new/new.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Article } from 'src/app/interfaces/interfaces';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+
 
 @Component({
   selector: 'app-new',
@@ -11,8 +13,15 @@ export class NewComponent implements OnInit {
   @Input() element: Article;
   @Input() index: number;
 
-  constructor() { }
+  constructor(
+    private iap: InAppBrowser
+  ) { }
 
   ngOnInit() {}
+
+  openNews() {
+    console.log('new', this.element.url);
+    const browser = this.iap.create(this.element.url, '_system');
+  }
 
 }
